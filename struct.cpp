50fec3955b9a68e907882586f50e60c3ea6b51e5
linyangl <57623813+linyangl@users.noreply.github.com>
2019-11-14 10:04:46 +0800

    Create struct.cpp

---
+++ struct.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<stdint.h>
+#include<cstdint>
+#include<string>
+using namespace std;
+struct KVDB
+{
+	uint32_t  key_length;
+	uint32_t  value_length;
+	string key;
+	string value;
+}
+int main()
+{
+	
+} 
